2d7714288721fd66c3e243b0ad484853fefbbfdc
Андрій Рубан <dombldor15@gmail.com>
2025-04-11 20:12:52 +0300

    Created basic routing for app

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
+
+def home(request):
+    return HttpResponse("<h1>Hello World</h1>")
 # Create your views here.
